fd925c64c83ad85186b6ca0cb5560dd5271a5172
ssarkar551 <ssarkar551@gmail.com>
2024-01-22 20:19:21 +0530

    url change

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 import { mongoose } from "mongoose";
-//mongodb url: mongodb+srv://shadowFang09:JawYJDSOePKwnWV3@cluster0.x4py9o2.mongodb.net/
-mongoose.connect('mongodb+srv://shadowFang09:JawYJDSOePKwnWV3@cluster0.x4py9o2.mongodb.net/')
+
+mongoose.connect(process.env.MONGO_URL);
 const todoschema = mongoose.Schema({
     title: String,
     description: String,
